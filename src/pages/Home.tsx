204d08e0f7dbaa7d4988eb23a44fed837a43caf5
yooooonira <yooooonira@gmail.com>
2026-01-30 11:59:52 +0900

    feat: initialize frontend structure with routing and tailwind setup

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Home() {
+  return (
+    <div className="p-8">
+      <h1 className="text-2xl font-bold mb-4">Home</h1>
+
+    </div>
+  );
+}
+
+export default Home;
